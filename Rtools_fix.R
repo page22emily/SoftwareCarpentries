88963fd1e032acfa0bd15e56d246590cadd40637
Emily Page <70612593+page22emily@users.noreply.github.com>
2023-03-02 15:40:01 -0600

    Create Rtools_fix.R

---
+++ Rtools_fix.R
@@ -0,0 +1,6 @@
+#to get Rtools
+#this path is for Rtools42
+install.package("https://cran.r-project.org/bin/windows/Rtools/rtools42/files/rtools42-5355-5357.exe", repos= NULL, type= "source")
+
+#if it is timinig out 
+options(timeout= 10000)
